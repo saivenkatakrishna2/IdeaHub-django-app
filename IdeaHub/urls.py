1e46e8fc9021e6c49e9c1f77f6d6da39f10a7797
saivenkatakrishna2 <saivenkatakrishna.pallampati@unh.edu>
2025-04-02 14:55:07 -0400

    Add members app with basic view and URL configuration

--- IdeaHub/urls.py
+++ IdeaHub/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-   
+    
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 
 ]
