ff32b26383f1d05e7ef29fa1a1daba833ac7497b
saivenkatakrishna2 <saivenkatakrishna.pallampati@unh.edu>
2025-04-02 15:27:00 -0400

    added initial database file

--- IdeaHub/urls.py
+++ IdeaHub/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    
+    path('admin/', admin.site.urls),
+    path('', include('Hub.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
 
